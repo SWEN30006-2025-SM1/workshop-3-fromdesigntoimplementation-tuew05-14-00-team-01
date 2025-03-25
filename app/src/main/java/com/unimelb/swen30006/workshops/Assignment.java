19d5775cc5ac96c1cc8304e751fcd7df3ef7304b
Zishen-019 <zishenl@student.unimelb.edu.au>
2025-03-25 15:46:18 +1100

    update

--- app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
@@ -1,4 +1,17 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.Date;
+
 public class Assignment {
+    private String description;
+    private Date dueDate;
+    private int maxAttempts;
+    private String name;
+    private int assID;
+
+    public Submission[] invalidSubmissions() {
+    }
+
+    public Submission[] validSubmissions() {
+    }
 }
