6de98afe797466dfdd4c24489ca59fd9a3ab3a51
Charlie Young <charles.young@student.unimelb.edu.au>
2025-03-25 15:32:47 +1100

    Staff and Student implementation

--- app/src/main/java/com/unimelb/swen30006/workshops/Student.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -1,4 +1,23 @@
 package com.unimelb.swen30006.workshops;
 
-public class Student {
+public class Student extends Person{
+    private String email;
+    private String studentID;
+    private float grade;
+
+    public Student(String name, String email, String studentID){
+        super(name);
+        this.email = email;
+        this.studentID = studentID;
+        this.grade = (float) 0.5;
+    }
+
+    public void sendEmailMsg(String msg) {
+        // implement email sending
+        return;
+    }
+
+    public float totalGrade() {
+        return this.grade;
+    }
 }
